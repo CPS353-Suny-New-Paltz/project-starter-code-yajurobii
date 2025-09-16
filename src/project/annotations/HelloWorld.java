b68a150a48745322243bf5349d2394c3cbacb254
yajurobii <yajurobii@gmail.com>
2025-09-16 14:07:58 -0400

    Update HelloWorld.java

--- src/project/annotations/HelloWorld.java
+++ src/project/annotations/HelloWorld.java
@@ -2,7 +2,7 @@ package project.annotations;
 
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello, world!")
+    System.out.println("Hello, world!");
   }
 
 }
