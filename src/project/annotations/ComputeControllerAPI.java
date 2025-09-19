ec774ca7178c39a90ac29d629776dd472b1c5340
poninka <43103397+poninka@users.noreply.github.com>
2025-09-18 22:40:05 -0400

    Revert "ComputeControllerAPI (Conceptual Boundary) (#18)"
    This reverts commit 75ec9065172c6c9aa6519c22b57e1c4bb33544bc.

--- src/project/annotations/ComputeControllerAPI.java
+++
@@ -1,7 +0,0 @@
-package project.annotations;
-
-@ConceptualAPI
-public interface ComputeControllerAPI {
-  // Compute a result based on the request (single integer input).
-  ComputeResponse compute(ComputeRequest request);
-}
