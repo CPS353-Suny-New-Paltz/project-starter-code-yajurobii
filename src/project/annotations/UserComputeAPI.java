8f524d98fc433b18a7bfcd93471886a11d06bdf8
yajurobii <yajurobii@gmail.com>
2025-09-24 18:57:26 -0400

    Fixing UserComputeAPI and related files (#22)
    Fix to make sure UserComputeAPi does not have any void return values by shifting and adding some things.

--- src/project/annotations/UserComputeAPI.java
+++ src/project/annotations/UserComputeAPI.java
@@ -3,5 +3,5 @@ package project.annotations;
 @NetworkAPI
 public interface UserComputeAPI {
   //User submits a request to the system. The user designates where input comes from, where output goes, and delimiters  with both. 
-  void submit(UserSubmission submission);
+  UserSubResponse submit(UserSubmission submission);
 }
