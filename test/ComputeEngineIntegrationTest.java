9aee626ff32d80f677679911337b21753efbc511
yajurobii <yajurobii@gmail.com>
2025-09-27 21:08:26 -0400

    Create ComputeEngineIntegrationTest.java (#32)
    Adding integration test for the two components of the compute engine, using datastore in memory.

---
+++ test/ComputeEngineIntegrationTest.java
@@ -0,0 +1,42 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+import project.annotations.StorageRequest;
+import project.annotations.StorageResponse;
+import project.annotations.StoreStatus;
+
+public class ComputeEngineIntegrationTest {
+  @Test
+  public void testInMemoryDataStore() {
+    DataStoreInMemory store = new DataStoreInMemory();
+
+    // Act as if user entered "16"
+    StorageResponse inResp = store.insertRequest(new StorageRequest("16".getBytes()));
+
+    // Assert that the storage worked.
+    assertEquals(StoreStatus.SUCCESS, inResp.getStatus());
+    assertNotNull(inResp.getId());
+
+    // Fetch same value back using earlier assigned ID.
+    String loadedInput = new String(store.loadData(inResp.getId()));
+
+    // Check what's gotten back is what was stored.
+    assertEquals("16", loadedInput);
+
+    // Act as if engine found primes <= 16.
+    StorageResponse outResp = store.insertResult(new StorageRequest("2,3,5,7,11,13".getBytes()));
+
+    // Assert result storage worked.
+    assertEquals(StoreStatus.SUCCESS, outResp.getStatus());
+    assertNotNull(outResp.getId());
+
+    // Same as loadedInput but for result.
+    String loadedResult = new String(store.loadResult(outResp.getId()));
+
+    // Verify that expected result is gotten.
+    assertEquals("2,3,5,7,11,13", loadedResult);
+
+  }
+
+}
