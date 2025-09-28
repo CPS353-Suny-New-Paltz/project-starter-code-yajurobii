50bbe5558d94413c353665e9acf77f2d6b00ecb8
yajurobii <yajurobii@gmail.com>
2025-09-27 20:17:57 -0400

    Create ComputeControllerAPIImplementation.java
    Adding empty implementation of ComputeControllerAPI.

---
+++ src/project/annotations/ComputeControllerAPIImplementation.java
@@ -0,0 +1,9 @@
+package project.annotations;
+
+public class ComputeControllerAPIImplementation implements ComputeControllerAPI {
+  @Override
+  public ComputeResponse compute(ComputeRequest request) {
+    return null;
+  }
+
+}
