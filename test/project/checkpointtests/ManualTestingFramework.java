2cfc4f6f9a4f27f6cbfd15329ac5ba026749bb99
yajurobii <yajurobii@gmail.com>
2025-10-11 21:09:34 -0400

    Update to manual test

--- test/project/checkpointtests/ManualTestingFramework.java
+++ test/project/checkpointtests/ManualTestingFramework.java
@@ -2,15 +2,21 @@ package project.checkpointtests;
 
 import project.annotations.UserComputeAPI;
 import project.annotations.UserComputeAPIImplementation;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 import project.annotations.ComputeControllerAPI;
 import project.annotations.ComputeControllerAPIImplementation;
+import project.annotations.ComputeRequest;
+import project.annotations.ComputeResponse;
 import project.annotations.DataStoreComputeAPI;
 import project.annotations.DataStoreComputeAPIImplementation;
 import project.annotations.InputSource;
 import project.annotations.OutputSource;
 import project.annotations.Delimiter;
 import project.annotations.UserSubmission;
-import project.annotations.UserSubResponse;
 
 public class ManualTestingFramework {
 
@@ -22,14 +28,35 @@ public class ManualTestingFramework {
     ComputeControllerAPI computeEngine = new ComputeControllerAPIImplementation();
     UserComputeAPI coordinator = new UserComputeAPIImplementation(dataStore, computeEngine);
 
-    UserSubmission submission = new UserSubmission(new InputSource("file", INPUT), new OutputSource(OUTPUT),
-        new Delimiter(",", ":"));
+    try {
+      List<String> inputs = Files.readAllLines(Paths.get(INPUT));
+      StringBuilder allResults = new StringBuilder();
 
-    UserSubResponse result = coordinator.submit(submission);
+      for (String line : inputs) {
+        int n = Integer.parseInt(line.trim());
 
-    System.out.println("Computation has finished.");
-    System.out.println("Status: " + result.getStatus());
-    System.out.println("Result Id: " + result.getSubId());
+        
+        UserSubmission sub = new UserSubmission(new InputSource("file", INPUT), new OutputSource(OUTPUT),
+            new Delimiter(",", ":"));
+
+
+        dataStore.insertRequest(n);
+
+        
+        ComputeResponse result = computeEngine.compute(new ComputeRequest(n));
+        allResults.append(result.getResult()).append(",");
+      }
+
+      
+      if (allResults.length() > 0)
+        allResults.setLength(allResults.length() - 1);
+
+      Files.writeString(Paths.get(OUTPUT), allResults.toString());
+      System.out.println("Computation done, results written to " + OUTPUT);
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
 
   }
 }
