fcff74aedb7e7bb3ce1925637b3ac8102b6c1c3c
yajurobii <yajurobii@gmail.com>
2025-11-06 16:52:45 -0500

    Adding test harness files
    Filling TODO's with appropriate API in multiuser and calling method's on coordinator in user. Also adding testInputFile.

---
+++ test/testHarness/TestUser.java
@@ -0,0 +1,46 @@
+package testHarness;
+
+import java.io.File;
+import java.nio.file.Files;
+
+import project.annotations.Delimiter;
+import project.annotations.InputSource;
+import project.annotations.OutputSource;
+import project.annotations.SubmissionStatus;
+import project.annotations.UserComputeAPI;
+import project.annotations.UserSubResponse;
+import project.annotations.UserSubmission;
+
+public class TestUser {
+
+  // TODO 3: change the type of this variable to the name you're using for your
+  // @NetworkAPI interface; also update the parameter passed to the constructor
+  private final UserComputeAPI coordinator;
+
+  public TestUser(UserComputeAPI coordinator) {
+    this.coordinator = coordinator;
+  }
+
+  public void run(String outputPath) {
+    char delimiter = ';';
+    String inputPath = "test" + File.separatorChar + "testInputFile.test";
+
+    // TODO 4: Call the appropriate method(s) on the coordinator to get it to
+    // run the compute job specified by inputPath, outputPath, and delimiter
+    UserSubmission sub = new UserSubmission(new InputSource("file", inputPath), new OutputSource("file", outputPath),
+        new Delimiter(String.valueOf(delimiter), ":"));
+
+    UserSubResponse resp = coordinator.submit(sub);
+    if (resp != null && resp.getStatus() == SubmissionStatus.SUCCESS) {
+      try {
+        String out = Files.readString(new File(outputPath).toPath());
+        System.out.println("Thread output: " + out);
+      } catch (Exception e) {
+        throw new RuntimeException("Could not read output file: " + outputPath, e);
+      }
+    } else {
+      throw new RuntimeException("Submission failed for output: " + outputPath);
+    }
+  }
+
+}
